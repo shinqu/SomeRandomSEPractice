954bcf1566151b9e896ab681eea33e9080748692
Neha Beke <nehabeke@gmail.com>
2015-10-17 13:21:35 -0500

    Deleted main file from main package and modified main.java of default package.

--- CleanSweep/src/Main.java
+++ CleanSweep/src/Main.java
@@ -1,28 +1,11 @@
-import java.awt.BorderLayout;
-import java.awt.Label;
+import com.blackdroidstudios.cleansweep.gui.GUIControl;
 
-import javax.swing.JFrame;
-
-public class Main {
-	public static void main(String[] args){
-		//1. Create the frame.
-		JFrame frame = new JFrame("FrameDemo");
-
-		//2. Optional: What happens when the frame closes?
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		//3. Create components and put them in the frame.
-		Label label = new Label("Test");
-		frame.getContentPane().add(label, BorderLayout.CENTER);
-
-		//4. Size the frame.
-		frame.pack();
-
-		//5. Show it.
-		frame.setVisible(true);
-		
-		frame.setTitle("Clean Sweep");
-		
-		frame.setSize(1280, 720);
+public class Main 
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+		GUIControl ctrl = new GUIControl();
+		ctrl.initializeGUI();	
 	}
+	
 }
